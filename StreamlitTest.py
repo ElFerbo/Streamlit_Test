c2536448f48715151feb95966e6ff0d219b9c3d7
ElFerbo <57297160+ElFerbo@users.noreply.github.com>
2025-02-15 08:16:46 +0100

    Update StreamlitTest.py

--- StreamlitTest.py
+++ StreamlitTest.py
@@ -3,15 +3,16 @@ import pandas as pd
 
 st.title('Uber pickups in NYC')
 
+# Initialisierung des Zählers im session_state
+if 'count' not in st.session_state:
+    st.session_state.count = 0
 
 st.title('Counter Example')
-count = 0
 
-increment = st.button('Increment')
-if increment:
-    count += 1
+# Button zum Inkrementieren des Zählers
+if st.button('Increment'):
+    st.session_state.count += 1
 
-st.write('Count = ', count)
+st.write('Count = ', st.session_state.count)
 
 st.balloons()
-#st.success
